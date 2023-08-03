4d4962807c4b93a19ce6bafbe900b1060fcc8500
Kevin Bolger <kevinwtbolger@gmail.com>
2023-08-03 02:35:50 -0700

    Update: add --profile-name tag

--- delete/delete-dynamodb-table.sh
+++ delete/delete-dynamodb-table.sh
@@ -5,4 +5,4 @@ echo "Deleting dynamodb-table stack..."
 source ./util/set-variables.sh
 
 aws cloudformation delete-stack --stack-name $DYNAMODB_TABLE_STACK --profile $PROFILE_NAME
-aws cloudformation wait stack-delete-complete --stack-name $DYNAMODB_TABLE_STACK
+aws cloudformation wait stack-delete-complete --stack-name $DYNAMODB_TABLE_STACK --profile $PROFILE_NAME
--- delete/delete-iam-policy.sh
+++ delete/delete-iam-policy.sh
@@ -5,4 +5,4 @@ echo "Deleting iam-policy stack..."
 source ./util/set-variables.sh
 
 aws cloudformation delete-stack --stack-name $IAM_POLICY_STACK --profile $PROFILE_NAME
-aws cloudformation wait stack-delete-complete --stack-name $IAM_POLICY_STACK
+aws cloudformation wait stack-delete-complete --stack-name $IAM_POLICY_STACK --profile $PROFILE_NAME
