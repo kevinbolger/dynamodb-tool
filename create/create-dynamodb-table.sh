952b8e28550c75a7c30ed325dd58b1d1df043117
Kevin Bolger <kevinwtbolger@gmail.com>
2023-08-03 10:40:15 -0700

    update: change sleep to more accurate values
    Each stack takes different lengths of time.
    
    These new sleep times should give enought time for them to complete.

--- create/create-dynamodb-table.sh
+++ create/create-dynamodb-table.sh
@@ -6,5 +6,5 @@ source ./util/set-variables.sh
 
 aws cloudformation create-stack --stack-name $DYNAMODB_TABLE_STACK --template-body file://./create/create-dynamodb-table.yaml --parameters ParameterKey=TableName,ParameterValue=$DYNAMODB_TABLE_NAME --profile $PROFILE_NAME > /dev/null 2>&1
 
-sleep 10
+sleep 20
 aws cloudformation wait stack-create-complete --stack-name $DYNAMODB_TABLE_STACK --profile $PROFILE_NAME
