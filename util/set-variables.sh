b1b4355cb7ed681263ba2bde58944a2db2efd27b
Kevin Bolger <kevinwtbolger@gmail.com>
2023-08-03 02:12:48 -0700

    Init: set-variables.sh

---
+++ util/set-variables.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Set variables
+
+PROFILE_NAME=my-aws-profile
+
+IAM_POLICY_STACK=my-iam-policy-stack
+IAM_POLICY_NAME=my-iam-policy
+
+DYNAMODB_TABLE_STACK=my-dynamodb-table-stack
+DYNAMODB_TABLE_NAME=my-dynamodb-table
+
+IAM_USER_STACK=my-iam-user-stack
+IAM_USER_NAME=my-iam-user
