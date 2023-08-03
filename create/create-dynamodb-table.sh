beceaa10fb2d130c6351e2736b06741311e2e481
Kevin Bolger <kevinwtbolger@gmail.com>
2023-08-03 02:36:18 -0700

    Update: remove superflupus --no-paginate tag

--- create/create-dynamodb-table.sh
+++ create/create-dynamodb-table.sh
@@ -6,4 +6,4 @@ source ./util/set-variables.sh
 
 aws cloudformation create-stack --stack-name $DYNAMODB_TABLE_STACK --template-body file://./create/create-dynamodb-table.yaml --parameters ParameterKey=TableName,ParameterValue=$DYNAMODB_TABLE_NAME --profile $PROFILE_NAME > /dev/null 2>&1
 
-aws cloudformation wait stack-create-complete --stack-name $DYNAMODB_TABLE_STACK --profile $PROFILE_NAME --no-paginate
+aws cloudformation wait stack-create-complete --stack-name $DYNAMODB_TABLE_STACK --profile $PROFILE_NAME
