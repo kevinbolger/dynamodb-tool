875f915c2f6bdfb4e5e46a48623aace609a850d3
Kevin Bolger <kevinwtbolger@gmail.com>
2023-08-03 10:36:16 -0700

    feat: add sleep before waitinf for stack creation
    The wait command only checks every 30 seconds.
    
    If it runs too soon, you must wait 30 seconds.
    
    Adding sleep so it gives stacks a chance to create.
    
    This should actually increase speed of bash script.
    
    Less waiting.

--- create/create-iam-policy.sh
+++ create/create-iam-policy.sh
@@ -6,4 +6,5 @@ source ./util/set-variables.sh
 
 aws cloudformation create-stack --stack-name $IAM_POLICY_STACK --template-body file://./create/create-iam-policy.yaml --parameters ParameterKey=IAMPolicyName,ParameterValue=$IAM_POLICY_NAME --profile $PROFILE_NAME --capabilities CAPABILITY_NAMED_IAM > /dev/null 2>&1
 
+sleep 10
 aws cloudformation wait stack-create-complete --stack-name $IAM_POLICY_STACK --profile $PROFILE_NAME
--- create/create-iam-user.sh
+++ create/create-iam-user.sh
@@ -6,4 +6,5 @@ source ./util/set-variables.sh
 
 aws cloudformation create-stack --stack-name $IAM_USER_STACK --template-body file://./create/create-iam-user.yaml --parameters ParameterKey=IAMUserName,ParameterValue=$IAM_USER_NAME --profile $PROFILE_NAME --capabilities CAPABILITY_NAMED_IAM > /dev/null 2>&1
 
+sleep 10
 aws cloudformation wait stack-create-complete --stack-name $IAM_USER_STACK --profile $PROFILE_NAME
